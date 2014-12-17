8ff18e027a59d9c563f6a17ac12f16737ae1748f
Ansgar Gerlicher <gerlicher@hdm-stuttgart.de>
2014-12-17 12:55:00 +0100

    added basic swift project

---
+++ SwiftProject/SwiftProject/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  SwiftProject
+//
+//  Created by Ansgar Gerlicher on 12.12.14.
+//  Copyright (c) 2014 Ansgar Gerlicher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var enterNameTextField: UITextField!
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+ 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        messageLabel.hidden = false;
+    }
+
+}
+
